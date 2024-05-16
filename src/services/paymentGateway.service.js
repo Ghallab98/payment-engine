5cce40d2d703f6552df528b194d8b72f71c60ed1
Mohamed Ghallab <ghallab98@gmail.com>
2024-05-16 19:12:53 +0300

    feat(payment): add the payment gateway mock service

---
+++ src/services/paymentGateway.service.js
@@ -0,0 +1,32 @@
+class PaymentGatewayService {
+  constructor() {
+    this.mockedGateway1 = {
+      name: "Mocked Gateway 1",
+      apiKey: "mocked_api_key_1",
+    };
+    this.mockedGateway2 = {
+      name: "Mocked Gateway 2",
+      apiKey: "mocked_api_key_2",
+    };
+  }
+
+  async processPaymentSuccess(amount) {
+    console.log(`Processing successful payment of $${amount}`);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return {
+      success: true,
+      message: "Payment processed successfully",
+    };
+  }
+
+  async processPaymentDecline(amount) {
+    console.log(`Processing declined payment of $${amount}`);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return {
+      success: false,
+      message: "Payment declined",
+    };
+  }
+}
+
+module.exports = PaymentGatewayService;
